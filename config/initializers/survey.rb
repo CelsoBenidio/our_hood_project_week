bd7c85a6100d1c94763d622063f7dc2987902ea9
Anuar Mukhambetzhanov <anuar.mukhambetzhanov@gmail.com>
2020-04-20 20:28:40 +0300

    created prelimenary survey

---
+++ config/initializers/survey.rb
@@ -0,0 +1,22 @@
+SURVEY = {
+  QUESTIONS: [
+    {
+      content: 'How old are you?',
+      id: 'age',
+      options: [
+        18,
+        25,
+        50
+      ]
+    },
+    {
+      content: 'Are you male or female?',
+      id: 'gender',
+      options: [
+        'male',
+        'female'
+      ]
+    }
+  ]
+}
+
