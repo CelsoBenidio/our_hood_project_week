9d9d107463ac09430de3abcb7bd3d8af3f2b05fa
Egecan Citim <egecancitim@gmail.com>
2020-04-21 15:36:51 +0300

    categories created, cart_product controller created, and some css work in products

---
+++ app/controllers/cart_products_controller.rb
@@ -0,0 +1,20 @@
+class CartProductsController < ApplicationController
+  def create
+    @product = Product.find(cart_product_params[:product_id])
+    @cart = current_user.cart
+    CartProduct.create(product: @product, cart: @cart)
+
+    redirect_to products_path
+  end
+
+  def destroy
+    # /cart_products/1 -> 1 is the id of CartProduct
+    @cart_product = CartProduct.find(params[:id])
+    @cart_product.destroy
+    redirect_to products_path
+  end
+
+  def cart_product_params
+    params.require(:cart_product).permit(:product_id)
+  end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :order_products
   has_many :orders, through: :order_products
+
+  has_many :cart_products, dependent: :destroy
+  has_many :carts, through: :cart_products
+
+  enum category: CATEGORIES
 end
