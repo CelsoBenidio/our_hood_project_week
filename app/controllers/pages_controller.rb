985439c00e211b2428afb31c22b3447aebe556c9
CelsoBenidio <49327194+CelsoBenidio@users.noreply.github.com>
2020-04-22 22:12:57 +0300

    Revert "Revert "added about us page, revised footer, revised banner and replaced imag…""

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :how_it_works]
+  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:how_it_works]
+  skip_before_action :authenticate_user!, only: [:about_us]
 
   def home
   end
 
   def how_it_works
+  end
+
+  def about_us
   end
 end
