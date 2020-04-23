7630f0cc8bdf9ffb7b14ddfba63a856049f7b7e4
Ulas <uli.will.code@protonmail.com>
2020-04-23 18:35:34 +0300

    login done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'registrations/create'
+  get 'sessions/create'
   get 'preferences/new'
   get 'how-it-works', to: 'pages#how-it-works'
   get 'about-us', to: 'pages#about-us'
-  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks",
+                                   sessions: "sessions",
+                                   registrations: "registrations"}
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :preferences, only: [:new, :create]
