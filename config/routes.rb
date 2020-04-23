b80519812440738430e26da361a323ec29412582
Ulas <uli.will.code@protonmail.com>
2020-04-23 13:18:54 +0300

    google login is done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'preferences/new'
   get 'how-it-works', to: 'pages#how-it-works'
   get 'about-us', to: 'pages#about-us'
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :preferences, only: [:new, :create]
