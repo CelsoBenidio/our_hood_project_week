7e231bc8115614345f1aa62606cd4ba6e7667251
Andrea Rocca <andrea.rocca3@gmail.com>
2020-04-24 15:24:37 +0200

    changed some workflows

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,56 +13,56 @@ class ApplicationController < ActionController::Base
     if current_user
       if session[:guest_user_id] && session[:guest_user_id] != current_user.id
         logging_in if is_new
-          # reload guest_user to prevent caching problems before destruction
-          guest_user(with_retry = false).try(:reload).try(:destroy)
-          session[:guest_user_id] = nil
-        end
-        current_user
-      else
-        guest_user
+        # reload guest_user to prevent caching problems before destruction
+        guest_user(with_retry = false).try(:reload).try(:destroy)
+        session[:guest_user_id] = nil
       end
-    end
-
-    # find guest_user object associated with the current session,
-    # creating one as needed
-    def guest_user(with_retry = true)
-      # Cache the value the first time it's gotten.
-      @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
-
-    rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
-     session[:guest_user_id] = nil
-     guest_user if with_retry
-   end
-
-   private
-
-    # called (once) when the user logs in, insert any code your application needs
-    # to hand off from guest_user to current_user.
-    def logging_in
-      # For example:
-      # guest_comments = guest_user.comments.all
-      # guest_comments.each do |comment|
-        # comment.user_id = current_user.id
-        # comment.save!
-      # end
-      guest_user.preference.update user_id: current_user.id if guest_user.preference
-      guest_cart_box_id = guest_user.cart.box_id
-      guest_cart_products = guest_user.cart.products
-      current_cart = current_user.cart
-      current_cart.box_id = guest_cart_box_id
-      current_cart.products = guest_cart_products
-      current_cart.save
-      guest_user.cart.destroy
-
-    end
-
-    def create_guest_user
-      u = User.new(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(100)}@example.com")
-      u.save!(:validate => false)
-      session[:guest_user_id] = u.id
-      u
+      current_user
+    else
+      guest_user
     end
   end
+
+  # find guest_user object associated with the current session,
+  # creating one as needed
+  def guest_user(with_retry = true)
+    # Cache the value the first time it's gotten.
+    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
+
+    rescue ActiveRecord::RecordNotFound # if session[:guest_user_id] invalid
+    session[:guest_user_id] = nil
+    guest_user if with_retry
+  end
+
+  private
+
+  # called (once) when the user logs in, insert any code your application needs
+  # to hand off from guest_user to current_user.
+  def logging_in
+    # For example:
+    # guest_comments = guest_user.comments.all
+    # guest_comments.each do |comment|
+      # comment.user_id = current_user.id
+      # comment.save!
+    # end
+    guest_user.preference.update user_id: current_user.id if guest_user.preference
+    guest_cart_box_id = guest_user.cart.box_id
+    guest_cart_products = guest_user.cart.products
+    current_cart = current_user.cart
+    current_cart.box_id = guest_cart_box_id
+    current_cart.products = guest_cart_products
+    current_cart.save
+    guest_user.cart.destroy
+
+  end
+
+  def create_guest_user
+    u = User.new(:name => "guest", :email => "guest_#{Time.now.to_i}#{rand(100)}@example.com")
+    u.save!(:validate => false)
+    session[:guest_user_id] = u.id
+    u
+  end
+end
 
 # 1. User add product to cart
 # 2. Click the proceed to checkout button
