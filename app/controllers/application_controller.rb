eca7306fb987c711b53fc8ffccd19c2c303e930f
Anuar Mukhambetzhanov <anuar.mukhambetzhanov@gmail.com>
2020-04-29 13:26:11 +0300

    finished adding avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   before_action :authenticate_user!
 
   before_action :current_or_guest_user
@@ -78,6 +81,13 @@ class ApplicationController < ActionController::Base
 
     def skip_pundit?
       devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^omniauth$)/
+    end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:avatar, :email, :password)}
+      # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
     end
 end
 
