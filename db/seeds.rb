708ff79749d42e70ba2820f8f804f2f00607d841
Andrea Rocca <andrea.rocca3@gmail.com>
2020-04-30 11:29:27 +0200

    commented out picture

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ puts "Boxes created"
 
 # 5 - Create Product
 olive_oil = Product.create(name: "Olive Oil", price: 45, description: "Turkey's sea shore has amazing olive trees, and this olive oil comes from nature's itself.", category: "non-alcholic beverages")
-comb_honey = Product.create(name: "Comb Honey", price: 89, description: "Fresh Comb Honey right from the hive.", category: "food")
+# comb_honey = Product.create(name: "Comb Honey", price: 89, description: "Fresh Comb Honey right from the hive.", category: "food")
 turkish_delight = Product.create(name: "Turkish Delight", price: 20, description: "Turkish delight or lokum is a family of confections based on a gel of starch and sugar.", category: "food")
 turnip_juice = Product.create(name: "Turnip Juice", price: 15, description: "Shalgam is a traditional beverage from the southern Turkey. It is drunk after eating Kebab.", category: "non-alcholic beverages")
 prayer_beads = Product.create(name: "Prayer Beads", price: 30, description: "Prayer beads are used by members of various religious traditions to mark the repetitions of prayers.", category: "accessories")
@@ -43,8 +43,8 @@ date_fruit = Product.create(name: "Date", price: 45, description: "Dry or soft d
 # product photos
 photo_oil = URI.open('https://www.pngarts.com/files/3/Olive-Oil-PNG-Transparent-Image.png')
 olive_oil.photo.attach(io: photo_oil, filename: 'oil.png', content_type: 'image/png')
-photo_honey = URI.open('https://www.stickpng.com/assets/images/5a188c3e8d421802430d2d19.png')
-comb_honey.photo.attach(io: photo_honey, filename: 'honeycomb.png', content_type: 'image/png')
+# photo_honey = URI.open('https://www.stickpng.com/assets/images/5a188c3e8d421802430d2d19.png')
+# comb_honey.photo.attach(io: photo_honey, filename: 'honeycomb.png', content_type: 'image/png')
 photo_delight = URI.open('https://img.pngio.com/turkish-delight-png-2-png-image-delight-png-209_159.png')
 turkish_delight.photo.attach(io: photo_delight, filename: 'turkish_delight.png', content_type: 'image/png')
 photo_turnip = URI.open('https://pngimage.net/wp-content/uploads/2018/06/%C5%9Falgam-suyu-png-2.png')
