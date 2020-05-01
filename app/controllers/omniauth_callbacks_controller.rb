4182b8dc6a124070ab688e1152ca72f3ddb157d6
Andrea Rocca <andrea.rocca3@gmail.com>
2020-05-01 11:06:10 +0200

    fixed social login bug

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,29 +6,39 @@ class OmniauthCallbacksController < ApplicationController
   def google_oauth2
     auth = request.env["omniauth.auth"]
 
-    user = User.where(provider: auth["provider"], uid: auth["uid"])
-            .first_or_initialize(email: auth["info"]["email"])
-    user.name ||= auth["info"]["name"]
-    user.password = SecureRandom.urlsafe_base64
-    user.save!
+    if User.where(provider: "facebook", email: auth["info"]["email"])
+      redirect_back(fallback_location: root_path)
+      flash[:alert] = "User not found"
+    else
+      user = User.where(provider: auth["provider"], uid: auth["uid"])
+              .first_or_initialize(email: auth["info"]["email"])
+      user.name ||= auth["info"]["name"]
+      user.password = SecureRandom.urlsafe_base64
+      user.save!
 
-    user.remember_me = true
-    sign_in(:user, user)
-    redirect_to after_sign_in_path_for(user)
+      user.remember_me = true
+      sign_in(:user, user)
+      redirect_to after_sign_in_path_for(user)
+    end
   end
 
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    auth = request.env["omniauth.auth"]
-
-    @user = User.from_omniauth(auth)
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      flash[:alert] = "You signed in successfully!"
+    if User.where(provider: "google_oauth2", email: auth["info"]["email"])
+      redirect_back(fallback_location: root_path)
+      flash[:alert] = "User not found"
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
+      auth = request.env["omniauth.auth"]
+
+      @user = User.from_omniauth(auth)
+
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+        flash[:alert] = "You signed in successfully!"
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
   end
 
